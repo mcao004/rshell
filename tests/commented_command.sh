0b433fad4c21a1b01d2d7ba899b75fa4664459ed
thaba002 <thaba002@ucr.edu>
2016-11-02 23:12:24 -0700

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -5,3 +5,6 @@ echo hub || #flame echo tub ; exit
 #exit
 #exit; exit
 echo hello&& #exit; hello
+echo hello #turtle ; ls -a || exit
+ls -a #opens files
+mkdir "comment file" && ls comment file #checksfile; exit
