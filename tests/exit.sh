3d0c3ff8de1d0e0b4cc57c42bc2fd8322b3ab3d0
thaba002 <thaba002@ucr.edu>
2016-11-02 21:42:12 -0700

    Create exit.sh

---
+++ tests/exit.sh
@@ -0,0 +1,8 @@
+exit
+exit;exit
+echo "hi|| exit && exit
+echo exit; mkdir exit; ls exit && exit
+echo "laser" tag; exit
+mkdir file.txt || exit
+ls -bdbdf && exit
+echo "family guy" exit || ls -a && exit
