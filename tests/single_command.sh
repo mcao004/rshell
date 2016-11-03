7464d92c2fdc069e9ee6de5a10bcc6f3d5ec8b15
thaba002 <thaba002@ucr.edu>
2016-11-02 21:32:01 -0700

    Update single_command.sh

---
+++ tests/single_command.sh
@@ -0,0 +1,30 @@
+echo hello; ls -a || mkdir hello.txt && ls -l; exit
+ls-a; echo "howdy" || exit && mkdir "folder"
+ls -a; ls -l && ls zszszdszsfz && echo hi
+exit
+mkdir red ||ls -a ||ls ||echo ||&& exit
+exit ||ls &&echo 55;
+echo 67 &&ls -w
+exit ;exit;exit
+echo echo;ls src;ls -a; mkdir src
+ls echo mkdir
+mkdir "lslslslslslslslslslslslsls.gsgd"
+echo "CS100: assignment"
+echo "Happy birthday to you";ls -a;mkdir "birthday.txt"
+echo tmorrow && mkdir tuesday&& ls tuesday
+echo exit
+echo howdy ||ls -a||mkdir firecracker
+echo echo
+echo "im running"
+echo im running
+echo im running;ls -a||echo "echo" echo
+ls -a||ls -l&&ls -a
+echo "exit" exit "exit"
+mkdir "echo.exit"
+mkdir "last file"
+ls -a
+la -l
+exit||exit&&exit
+htrhfghrdgdfgdf||echo did it work
+echo echo||mkdir echo|| ls echo
+exit||exit
