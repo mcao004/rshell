e437967be3ce5f28159b649d5efdecef414c2d16
thaba002 <thaba002@ucr.edu>
2016-11-02 23:10:53 -0700

    Update exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -6,3 +6,6 @@ echo "laser" tag; exit
 mkdir file.txt || exit
 ls -bdbdf && exit
 echo "family guy" exit || ls -a && exit
+echo "howdy partner"; exit
+mkdir exit; ls exit; exit
+mkdir "exit"; ls "exit" && exit
