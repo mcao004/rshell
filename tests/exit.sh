23eabac878e6dccbe915275850ba0a38a67c5bef
thaba002 <thaba002@ucr.edu>
2016-11-18 17:55:01 -0800

    Update exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -9,3 +9,13 @@ echo "family guy" exit || ls -a && exit
 echo "howdy partner"; exit
 mkdir exit; ls exit; exit
 mkdir "exit"; ls "exit" && exit
+(exit)
+echo hello && (echo hello && exit)
+(echo hello && ls -a) ; exit
+ test -e /test/file/path && exit
+( test -e /test/file/path ) ;exit
+exit ; exit (exit)
+( test -e /test/file/path && echo “boom") ; exit
+echo exit; exit
+(echo l) || mkdir el; exit
+
