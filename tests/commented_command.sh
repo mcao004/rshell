20d9b05786f3ac8f1f99308a86682fab7debbfa2
thaba002 <thaba002@ucr.edu>
2016-11-18 17:59:56 -0800

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -8,3 +8,10 @@ echo hello&& #exit; hello
 echo hello #turtle ; ls -a || exit
 ls -a #opens files
 mkdir "comment file" && ls comment file #checksfile; exit
+(#hello echo hello) ; ls -a
+test -e /test/file/path && ls -a #v
+(echo b || echo c #go) || ls -l
+echo c || #vre echo d
+ test -e /test/file/path && #teest ls -b
+ 
+
