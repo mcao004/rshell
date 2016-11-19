bf2a7ed8a05053086d84c6f58b4f6d8ea337a946
thaba002 <thaba002@ucr.edu>
2016-11-18 17:40:02 -0800

    Create test_test.sh

---
+++ tests/test_test.sh
@@ -0,0 +1,15 @@
+test -e /test/file/path
+[ -e /test/file/path ] 
+[ -f /test/file/path ] 
+test -f /test/file/path
+test -d /test/file/path
+[ -f /test/file/path ] 
+[  /test/file/path ] 
+test /test/file/path
+echo test && test -e /test/file/path
+ls -1 || test -d /test/file/path
+[ -f /test/file/path ] ; mkdir net
+[ -f /test/file/path ] && [ -f /test/file/path ] &&  echo fire
+mkdir laser tag ;test -d /test/file/path && test /test/file/path
+ls -a ; test -d /test/file/path && echo test && test -e /test/file/path
+echo running out of ideas ; test /test/file/path
