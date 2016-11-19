26caca4dc94b81dcbb8dc6324a0c7b342d3a6d1a
thaba002 <thaba002@ucr.edu>
2016-11-18 17:51:49 -0800

    Update precedence_test.sh

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -4,3 +4,7 @@ echo hello || (echo a && echo b || ls-a)
 ehco 56|| (mkdir fail && echo fail && echo failure)
 ls -a || ( echo hello world || mkdir cat && conrats) || echo we did it
 (echo hello) || (echo flavor && echo boop) || ( test -e /test/file/path )
+(ls -a ||   test -e /test/file/path && echo  "times up" || ls -a) && (echo b; echo c)
+(echo b);echo c|| (echo d)
+(echo hello ; echo world) || ls -a
+(echo abc) || ( ls -a) && echo v
